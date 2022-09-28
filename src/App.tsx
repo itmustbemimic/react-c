be66c0231238b98f997eaddc8f4b263b06f4d5d5
mimic <itmustbemimic@gmail.com>
2022-09-28 21:56:01 +0900

    s

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
                 <div>user balance: {balance}</div>
                 <button onClick={connectWallet}>connect</button>
                 <button onClick={sendTransaction}>send</button>
-                <div>update test111</div>
+                <div>update test1111</div>
             </header>
         </div>
     );
